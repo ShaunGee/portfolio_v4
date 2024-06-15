e2eb9ac0484cc0ec5a14cc4290a6a653b17d6dcf
Shaun Gumbaketi <shaungumbaketi@gmail.com>
2024-06-16 09:04:00 +1000

    homepage for mobile created, still need to populate with pojects and blogs app

--- portfolio_v4/settings.py
+++ portfolio_v4/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '192.168.1.104',
+]
 
 
 # Application definition
--- portfolio_v4/views.py
+++ portfolio_v4/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'base.html')
+    return render(request, 'home.html')
 
